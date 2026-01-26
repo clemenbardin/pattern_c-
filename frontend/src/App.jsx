5417b7cd725065d5c996a69aa6eab3f2bf4f13f7
clement.bardin <clement.bardin@ecole-isitech.fr>
2026-01-26 12:06:38 +0100

    [INIT] Add Docker support for backend and frontend applications

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1>Mon App</h1>
+      <p>Application en cours de développement</p>
+    </div>
+  )
+}
+
+export default App
